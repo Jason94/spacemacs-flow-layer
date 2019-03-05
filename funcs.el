43ae97de9a4ad125a54129ac90a2ded451f2e879
Jason Walker <jason.walker@wicourts.gov>
2019-03-05 13:06:12 -0600

    Got flycheck working in web-mode

--- funcs.el
+++ funcs.el
@@ -1,7 +1,23 @@
 ;; flow
 
 (defun spacemacs//flow-use-flow-from-node-modules ()
-  )
+  (let* ((os (pcase system-type
+               ('darwin "osx")
+               ('gnu/linux "linux64")
+               (_ nil)))
+         (root (locate-dominating-file 
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (global-flow (executable-find "flow"))
+         (local-flow (expand-file-name "node_modules/.bin/flow"
+                                       root))
+         (flow (if (file-executable-p local-flow)
+                   local-flow
+                 global-flow)))
+    (when (and flow (file-executable-p flow))
+      ;;(setq-local company-flow-exeuctable flow)
+      (setq-local flow-minor-default-binary flow)
+      (setq-local flycheck-javascript-flow-executable flow))))
 
 ;; flycheck
 
@@ -9,7 +25,7 @@
   (let* ((root (locate-dominating-file
                 (or (buffer-file-name) default-directory)
                 "node_modules"))
-         (global-eslint (execuable-find "eslint"))
+         (global-eslint (executable-find "eslint"))
          (local-eslint (expand-file-name "node_modules/.bin/eslint"
                                          root))
          (eslint (if (file-executable-p local-eslint)
--- packages.el
+++ packages.el
@@ -2,24 +2,35 @@
 
 (setq flow-packages
   '(flow-minor-mode
+    web-mode
+    company-mode
+    flycheck
     (company-flow :toggle (configuration-layer/package-usedp 'company))
     (flycheck-flow :toggle (configuration-layer/package-usedp 'flycheck))
-    web-mode
     ))
 
 (defun flow/init-flow-minor-mode ()
   (use-package flow-minor-mode
     :defer t))
 
-(defun flow/init-flycheck-flow  ()
-  (use-package flycheck-flow
-    :defer t))
+(defun flow/post-init-flow-minor-mode ()
+  (add-hook 'flow-minor-mode-hook #'spacemacs//flow-use-flow-from-node-modules t))
 
-(defun flow/post-init-flycheck ()
+(defun flow/init-flycheck-flow  ()
+  (use-package flycheck-flow))
+
+(defun flow/post-init-flycheck-flow ()
   (with-eval-after-load 'flycheck
-    (flycheck-add-mode 'javascript-flow 'flow-minor-mode)
-    (flycheck-add-mode 'javascript-eslint 'flow-minor-mode)
-    (flycheck-add-next-checker 'javascript-flow 'javascript-eslint))
+    (with-eval-after-load 'flycheck-flow
+      (flycheck-add-mode 'javascript-flow 'flow-minor-mode)
+      (flycheck-add-mode 'javascript-eslint 'flow-minor-mode)
+      (flycheck-add-next-checker 'javascript-flow 'javascript-eslint)))
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
   (add-hook 'flow-minor-mode-hook #'spacemacs//flow-use-eslint-from-node-modules)
   (spacemacs/add-flycheck-hook 'flow-minor-mode))
-  
+
+(defun flow/post-init-web-mode ()
+  (add-hook 'web-mode-hook 'flow-minor-enable-automatically)
+  (spacemacs/add-flycheck-hook 'web-mode))
